5441409c5cdd119f96028fd31e7e0e317c25fbe6
Júlio Evêncio <65574850+JulioEvencio@users.noreply.github.com>
2020-12-06 17:03:39 -0300

    Recomeçando código main

--- Foge_Foge/main.c
+++ Foge_Foge/main.c
@@ -20,77 +20,44 @@ int main()
 {
     //  Definindo semente para a funcao rand
     srand(time(NULL));
-
-    //  Variavel que controla o loop principal
-    int opcao;
     
-    //  Loop do programa
+    //  Leitura do mapa na matriz
+    ler_mapa(&m, MAPA_LEVEL_1);
+
+    //  Identificando os elementos no mapa
+    identificar_elementos(&m);
+
+    //  Localizando personagem
+    localizar_personagem();
+
+    //  Localizando fantasmas
+    localizar_fantasmas();
+
+    //  Zerando level
+    m.level = LEVEL_1;
+
+    //  Loop do jogo
     do
     {
-        //  Limpando tela
+        //  Limpando a tela
         system("clear");
-        //  Imprimindo menu principal
-        imprimir_menu();
-        //  Leitura da opcao
-        scanf("%d", &opcao);
-        setbuf(stdin, NULL);
-        //  Verificando a opcao
-        switch(opcao)
-        {
-            case(RODAR_JOGO):
-                //  Code
-                break;
-            case(2):
-                printf("EM BREVE! \n");
-                break;
-            case(SAIR_JOGO):
-                printf("Obrigado por jogar! \n");
-                opcao = SAIR_JOGO;
-                break;
-            default:
-                printf("Opcao invalida! \n");
-                break;
-        }
+        //  Imprimindo mapa na tela
+        imprimir_mapa(&m);
+        //  Variavel do comando do usuario
+        char comando;
+        //  Leitura do comando do usuario
+        scanf(" %c", &comando);
+        //  Movimentando personagem
+        movimentar_personagem(comando);
+        //  Movimento dos fantasmas
+        movimentar_fantasmas(0);
+        //  Verificando se o usuaio passou de level
+        m.level = completar_level(m.level);
 
-        //  Leitura do mapa na matriz
-        ler_mapa(&m, MAPA_LEVEL_1);
+    }while(verificar_gameover(m.level));
 
-        //  Identificando os elementos no mapa
-        identificar_elementos(&m);
-
-        //  Localizando personagem
-        localizar_personagem();
-
-        //  Localizando fantasmas
-        localizar_fantasmas();
-
-        //  Zerando level
-        m.level = LEVEL_1;
-
-        //  Loop do jogo
-        while(verificar_gameover(m.level))
-        {
-            //  Limpando a tela
-            system("clear");
-            //  Imprimindo mapa na tela
-            imprimir_mapa(&m);
-            //  Variavel do comando do usuario
-            char comando;
-            //  Leitura do comando do usuario
-            scanf(" %c", &comando);
-            //  Movimentando personagem
-            movimentar_personagem(comando);
-            //  Movimento dos fantasmas
-            movimentar_fantasmas(0);
-            //  Verificando se o usuaio passou de level
-            m.level = completar_level(m.level);
-
-        }
-
-        //  Liberando espaco na memoria antes de encerra o programa
-        liberar_matriz(&m);
-
-    }while(opcao != 0);
+    //  Liberando espaco na memoria antes de encerra o programa
+    liberar_matriz(&m);
 
     return 0;
 }
